ce7873ec8235c1e9610eac039404f09044c1c382
Sperr <ed_sperr@hotmail.com>
2017-08-22 09:45:10 -0400

    Reoriented around comparisons between searches. Nicknames incorporated

--- yearframework.js
+++ yearframework.js
@@ -1,33 +1,32 @@
-var yearsearches = { counts:{}, searchterms:[] };
 var basecounts = {};
+var allcounts;
+
+var d = new Date();
+var thisYear = d.getFullYear();
+var startYear = 1945;
+var endYear = thisYear;
 
 var data;
 var options;
 
+
 $.get( "https://med-by-year.appspot.com/showbasecounts", function( data ) {
   //build out our data structure
-  var allCounts = data.counts;
-  for (var year in allCounts) {
-    yearsearches.counts[year] = { "total": allCounts[year] } ;
-  }
+  allCounts = data.counts;
 })
 .fail(function() {
   alert( "There was some sort of problem -- please reload the page" );
 });
 
-function yearsearch(term) {
+function yearsearch(searchkey, term) {
   urlstem = "https://med-by-year.appspot.com/search?q=";
   url = urlstem + encodeURIComponent(term);
   $.get( url, function( data ) {
     if (data.hasOwnProperty('counts')) {
-      yearsearches.searchterms.push(term);
-      myCounts = data.counts;
-      for (var year in myCounts) {
-        yearsearches.counts[year][term] = myCounts[year];
-      }
-      drawYearChart();
+      searches[searchkey].yearcounts = data.counts;
+      drawYearChart([searchkey]);
     } else {
-      alert("Nothing found for this search or too few results to chart by year. Please try again");
+      return "Nothing found for this search or too few results to chart by year. Please try again";
     }
   })
   .fail(function() {
@@ -35,21 +34,35 @@ function yearsearch(term) {
   });
   }
 
-  function drawYearChart() {
-    $("#yearresults").empty();
-
-    $( "#yrearesults" ).append( '<div class="panel panel-default">' );
-    $( "#yearresults .panel" ).append( '<div id="chart_div" class="panel-body">' );
-
+  function drawYearChart(searchNumbers) {
+    //takes an array of searches to compare...
+    var yearsearches = { counts:{}, searchterms:[] };
+    for (var year in allCounts) {
+      yearsearches.counts[year] = { "total": allCounts[year] } ;
+    }
+    for (i = 0; i < searchNumbers.length; i++) {
+      if (searches[searchNumbers[i]].nickname) {
+        searchname = '"' + searches[searchNumbers[i]].nickname + '"';
+      } else {
+        searchname =  searches[searchNumbers[i]].term;
+      }
+      yearsearches.searchterms.push(searchname);
+      myCounts = searches[searchNumbers[i]].yearcounts;
+      for (var year in myCounts) {
+        yearsearches.counts[year][searchname] = myCounts[year];
+      }
+    }
 
     data = new google.visualization.DataTable();
+
     data.addColumn('string', 'Year');
     for (i = 0; i < yearsearches.searchterms.length; i++) {
       data.addColumn('number', yearsearches.searchterms[i]);
     }
 
-
     for (var year in yearsearches.counts) {
+      if (Number(year) < startYear) { continue; }
+      if (Number(year) > endYear) { continue; }
       theseCounts = [year];
       for (i = 0; i < yearsearches.searchterms.length; i++) {
         var term = yearsearches.searchterms[i];
@@ -58,7 +71,8 @@ function yearsearch(term) {
           var proportion = Number(yearsearches.counts[year][term]) / yearTotal;
           construct = {
             v: proportion,
-            f: Number(yearsearches.counts[year][term]).toLocaleString() + " citations (out of " + yearTotal.toLocaleString() + " in " + year + ")"
+            f: yearsearches.counts[year][term] + " citations"
+            //f: Number(yearsearches.counts[year][term]).toLocaleString() + " citations (out of " + yearTotal.toLocaleString() + " in " + year + ")"
           }
           theseCounts.push(construct);
           //theseCounts.push(proportion);
@@ -72,15 +86,17 @@ function yearsearch(term) {
     options = {
           chart: {
             title: 'Proportion of citations in PubMed',
-            subtitle: 'proportion for each search by year, ' + $("[name=startyear]").val() + ' to ' + $("[name=endyear]").val()
+            subtitle: 'proportion for each search by year'
           },
-          width: mywidth,
-          height: myheight,
+          width: myyearwidth,
+          height: myyearheight,
           vAxis: {format: 'decimal',
                   title: 'proportion'},
         };
-
-    var chart = new google.charts.Line(document.getElementById('chart_div'));
+    $("#yearresults").empty();
+    //$( "#yrearesults" ).append( '<div class="panel panel-default">' );
+    //$( "#yearresults .panel" ).append( '<div id="chart_div" class="panel-body">' );
+    var chart = new google.charts.Line(document.getElementById('yearresults'));
 
     chart.draw(data,  google.charts.Line.convertOptions(options));
 
