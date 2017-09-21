89c9888ad3158a9d8d55fa341c964b6608d8cecf
Sperr <ed_sperr@hotmail.com>
2017-09-21 14:15:52 -0400

    Added more hedges

--- hedges.js
+++ hedges.js
@@ -21,4 +21,10 @@ var hedges = [
     "term": "Clinical Prediction Guides/Broad[filter]" },
   { "name": "Clinical Prediction Guides (narrow)",
     "term": "Clinical Prediction Guides/Narrow[filter]" },
+  { "name": "Qualitative",
+    "term": "interview*[Title/Abstract] OR interviews[MeSH:noexp] OR experience*[Text Word] OR qualitative[Title/Abstract]" },
+  { "name": "Costs",
+    "term": "'costs and cost analysis'[MeSH Term] OR costs[Title/Abstract] OR cost effective*[Title/Abstract]" },
+  { "name": "Economics ",
+    "term": "cost*[Title/Abstract] OR 'costs and cost analysis'[MeSH:noexp] OR cost benefit analys*[Title/Abstract] OR cost-benefit analysis[MeSH Term] OR health care costs[MeSH:noexp]" },
 ]
